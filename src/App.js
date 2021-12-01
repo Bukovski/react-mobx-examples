0a85d38170e2dee720b0b1913d2dda3bfe2bf8ff
Bukovski <kmanankov@bk.ru>
2021-12-01 11:20:00 +0300

    add: counter with store

--- src/App.js
+++ src/App.js
@@ -1,93 +1,14 @@
 import React from 'react';
-import { observer } from 'mobx-react';
-import { configure, makeAutoObservable } from 'mobx';
-
+import { configure } from 'mobx';
+import { Counter, counterState } from "./Counter";
 
 configure({ enforceActions: 'observed' });
 
 
-class TodoStore {
-  todos = [
-    { id: 1, title: "some text 1", completed: false, count: 4 },
-    { id: 2, title: "some text 12", completed: false, count: 2 },
-    { id: 3, title: "some text 123", completed: false, count: 8 },
-  ];
-  
-  constructor() {
-    makeAutoObservable(this) // автоматически настраивает store
-    
-    // { id: 7, title: { name: "some name", label: "text" }, completed: false },
-    // makeAutoObservable(this, {}, { deep: true }) // глубокое отслеживанеи вложенных данных
-    
-    // makeObservable(this, { todos: observable, addTodo: action, counter: completed }) // в ручную указываем к чему относить свойства класса
-  }
-  
-  addTodo(todo) {
-    this.todos.push(todo);
-  }
-  
-  removeTodo(id) {
-    this.todos = this.todos.filter(todo => todo.id !== id)
-  }
-  
-  completeTodo(id) {
-    this.todos = this.todos.map(todo => (todo.id === id) ? { ...todo, completed: !todo.completed } : todo)
-  }
-  
-  get counter() {
-    let count = 0;
-    
-    this.todos.forEach(todo => count += (todo.completed) ? todo.count : 0);
-    
-    return count;
-  }
-  
-  fetchUser() {
-    fetch("https://randomuser.me/api/")
-      .then(res => res.json())
-      .then(json => {
-        const res = json.results[ 0 ]
-        const transform = { id: res.login.md5, title: res.name.title + " " + res.name.first, completed: false, count: res.dob.age }
-        
-        // this.todos = [ ...this.todos, transform ];
-        this.todos.push(transform);
-      })
-  }
-}
-
-const store = new TodoStore();
-
-/**********/
-
-// observer - заставляет компонент обновиться если данные в store изменились
-const Todo = observer(() => {
-  return (
-    <div>
-      { store.counter }
-      
-      { store.todos.map(todo =>
-        <div className="todo" key={ todo.id }>
-          <input
-            type="checkbox"
-            checked={ todo.completed }
-            onChange={ () => store.completeTodo(todo.id) }
-          />
-          { todo.title }
-          <button onClick={ () => store.removeTodo(todo.id) }>X</button>
-        </div>
-      )}
-      
-      <button onClick={ () => store.fetchUser() }>More Users</button>
-    </div>
-  )
-})
-
-
 const App = () => {
-  
   return (
     <div className="app">
-      <Todo />
+      <Counter store={ counterState } />
     </div>
   );
 }
---
+++ src/Counter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { observable } from "mobx";
+import { observer } from "mobx-react";
+
+export const counterState = observable({
+	count: 0,
+});
+
+counterState.increment = function() {
+	this.count++;
+};
+
+counterState.decrement = function() {
+	this.count--;
+};
+
+
+export const Counter = observer((props) => {
+	const handleIncrement = () => { props.store.increment() };
+	const handleDecrement = () => { props.store.decrement() };
+	
+	return (
+		<div className="App">
+			
+			<h1>{ props.store.count }</h1>
+			
+			<button onClick={ handleDecrement }>-1</button>
+			<button onClick={ handleIncrement }>+1</button>
+		</div>
+	);
+})
+
